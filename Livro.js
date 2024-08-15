25ed167daaf4aa7566d678084420494804dd4ef0
Vinirocha388 <vinicius.a.rocha@aluno.senai.br>
2024-08-15 09:35:25 -0300

    First commit

---
+++ Livro.js
@@ -0,0 +1,68 @@
+ 
+ Livro
+ 
+ 
+ const livro = { 
+/    Titulo: '"Breve respostas para grandes questões" ',
+     Autor: ' Stephen Hawking',
+    Ano: ' 16/10/2018',
+ };
+
+ console.log('O livro: ' + livro.Titulo + 'De ' +  livro.Autor + ' foi lançado em ' + livro.Ano);
+
+
+//Filme
+
+
+const filme = {  
+    titulo: '"A teoria de tudo"',
+     diretor: 'James Marsh',
+     anoLancamento: '29/01/2015',
+ };
+ console.log(filme);
+
+
+//Música
+
+
+ const musica = {
+     titulo: 'Con te Partirò',
+     artista: 'Andrea Bocelli',
+    album: 'Bocelli'
+
+ };
+ console.log(musica);
+
+
+//Restaurante
+
+
+ const restaurante = {   
+     nome: 'Vovó Luiza Pizzaria',
+     tipoCozinha: 'Italiana',
+     local: 'Valinhos-SP, próximo ao CLT',
+ };
+ console.log(restaurante);
+
+
+
+ //Jogo de Video Game
+ 
+ const Jogo = {  
+     titulo: '8 Ball Pool',
+     desenvolvedor: 'Miniclip SA',
+     plataforma: 'IOS,Android e Navegador web',
+ };
+
+ console.log(Jogo);
+
+
+
+//Smartphone
+
+ const smartphone = {    
+    marca: 'ASUS',
+    modelo: 'Zenfone 8'
+    anoLancamento: 'Maio de 2021',
+};
+console.log(smartphone)
